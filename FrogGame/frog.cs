afe3a595d0f3c910565c046e72a028e65e503504
StefanHerald <s.herald@students.uu.nl>
2022-11-17 14:41:03 +0100

    added abstract frog

---
+++ FrogGame/frog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogGame
+{
+    internal abstract class Frog
+    {
+        protected float width, stability;
+        protected int flyCost, fireFlyCost;
+        protected List<subType> subTypes = new List<subType>();
+        protected List<status> statuses = new List<status>();
+        public enum subType
+        {
+            fly_catching,
+            ferocious,
+            resistant,
+            coldresistant,
+            flameresistant
+        }
+
+        public enum status
+        {
+            brittle,
+            frozen,
+            onfire, 
+        }
+    }
+}
